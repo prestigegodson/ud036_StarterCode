0d25eb5a1fcce16c60d7ebe176fdf32ccbd14a0c
prestigegodson <prestigegodson@gmail.com>
2017-11-05 22:26:20 +0100

    completed project movie trailers

---
+++ media.py
@@ -0,0 +1,13 @@
+class Movie():
+    
+    """
+    This class initializes the movie title, movie poster image,
+    and movie trailer youtube url.
+    These arguments must be passed when instantiating this class
+
+    """
+    
+    def __init__(self,title,poster_image_url,trailer_youtube_url):
+        self.title = title
+        self.poster_image_url = poster_image_url
+        self.trailer_youtube_url = trailer_youtube_url
