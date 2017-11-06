bc1abdf4f100ce3aee94df703b6da5af1f01c604
prestigegodson <prestigegodson@gmail.com>
2017-11-06 14:18:06 +0100

    updated code to follow PEP-8 Guideline

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,22 +1,25 @@
 import media
-import fresh_tomatoes;
+import fresh_tomatoes
 
 # instanting Movie class with movie properties
 kings_man = media.Movie(
     "Kingsman: The Golden Circle",
-    "https://upload.wikimedia.org/wikipedia/en/f/fb/Kingsman_The_Golden_Circle.png",
+    ("https://upload.wikimedia.org/wikipedia/en/f/fb/"
+     "Kingsman_The_Golden_Circle.png"),
     "https://www.youtube.com/watch?v=6Nxc-3WpMbg"
 )
 
 thor_ragnarok = media.Movie(
     "Thor: Ragnarok",
-    "https://upload.wikimedia.org/wikipedia/en/7/7d/Thor_Ragnarok_poster.jpg",
+    ("https://upload.wikimedia.org/wikipedia/en/7/7d/"
+     "Thor_Ragnarok_poster.jpg"),
     "https://www.youtube.com/watch?v=v7MGUNV8MxU"
 )
 
 justice_league = media.Movie(
     "Justice League",
-    "https://upload.wikimedia.org/wikipedia/en/thumb/3/31/Justice_League_film_poster.jpg/220px-Justice_League_film_poster.jpg",
+    ("https://upload.wikimedia.org/wikipedia/en/thumb/3/31/"
+     "Justice_League_film_poster.jpg/220px-Justice_League_film_poster.jpg"),
     "https://www.youtube.com/watch?v=r9-DM9uBtVI"
 )
 
