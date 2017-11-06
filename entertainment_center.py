5468fa037f0c90d0404c6fc92f207ea59c368657
prestigegodson <prestigegodson@gmail.com>
2017-11-06 09:35:38 +0100

    fixed issues raised during review

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,13 +1,31 @@
 import media
 import fresh_tomatoes;
 
-#instanting Movie class with movie properties
-kings_man = media.Movie("Kingsman: The Golden Circle","https://upload.wikimedia.org/wikipedia/en/f/fb/Kingsman_The_Golden_Circle.png","https://www.youtube.com/watch?v=6Nxc-3WpMbg")
-thor_ragnarok = media.Movie("Thor: Ragnarok","https://upload.wikimedia.org/wikipedia/en/7/7d/Thor_Ragnarok_poster.jpg","https://www.youtube.com/watch?v=v7MGUNV8MxU")
-justice_league = media.Movie("Justice League","https://upload.wikimedia.org/wikipedia/en/thumb/3/31/Justice_League_film_poster.jpg/220px-Justice_League_film_poster.jpg","https://www.youtube.com/watch?v=r9-DM9uBtVI")
+# instanting Movie class with movie properties
+kings_man = media.Movie(
+    "Kingsman: The Golden Circle",
+    "https://upload.wikimedia.org/wikipedia/en/f/fb/Kingsman_The_Golden_Circle.png",
+    "https://www.youtube.com/watch?v=6Nxc-3WpMbg"
+)
 
-#adding Movie object to movies list
-movies = [kings_man,thor_ragnarok,justice_league]
+thor_ragnarok = media.Movie(
+    "Thor: Ragnarok",
+    "https://upload.wikimedia.org/wikipedia/en/7/7d/Thor_Ragnarok_poster.jpg",
+    "https://www.youtube.com/watch?v=v7MGUNV8MxU"
+)
 
-#calling the open_movies_page function to display the movies trailers
+justice_league = media.Movie(
+    "Justice League",
+    "https://upload.wikimedia.org/wikipedia/en/thumb/3/31/Justice_League_film_poster.jpg/220px-Justice_League_film_poster.jpg",
+    "https://www.youtube.com/watch?v=r9-DM9uBtVI"
+)
+
+# adding Movie object to movies list
+movies = [
+    kings_man,
+    thor_ragnarok,
+    justice_league
+]
+
+# calling the open_movies_page function to display the movies trailers
 fresh_tomatoes.open_movies_page(movies)
